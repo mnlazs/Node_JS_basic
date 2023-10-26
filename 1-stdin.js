e554e88411bfa207f7ce29705644ae534fccff87
mnlazs <mnl.azs@gmail.com>
2023-10-25 22:05:25 -0500

    updated

--- 1-stdin.js
+++ 1-stdin.js
@@ -1,15 +1,13 @@
 // 1-stdin.js
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
 process.stdin.setEncoding('utf8');
-
 process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    process.stdout.write(`Your name is: ${chunk}`);
+  const tunombre = process.stdin.read();
+  if (tunombre !== null) {
+    process.stdout.write(`Your name is: ${tunombre}`);
   }
 });
 
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing\n');
 });
